2865dcf9f7ff1d765524f396b40c7a0aaa3e95f7
ashmitha-nayak <148947012+ashmitha-nayak@users.noreply.github.com>
2023-10-31 22:31:06 +0530

    Add files via upload

---
+++ ForLoopDemo.java
@@ -0,0 +1,14 @@
+package javacoreproject;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		
+		for(int count=1;count<=10;count++) {
+			
+			System.out.println("This is iteration : " +count);
+		}
+
+	}
+
+}
