9486bf8a9c87cb244c98559340b52370e29b513e
ashmitha-nayak <148947012+ashmitha-nayak@users.noreply.github.com>
2023-10-31 22:29:23 +0530

    Add files via upload

---
+++ LoopProgram.java
@@ -0,0 +1,17 @@
+package javacoreproject;
+
+public class LoopProgram {
+
+	public static void main(String[] args) {
+		
+		int count=1;
+		
+		while(count <= 10) {
+			
+			System.out.println("This is iteration : " +count);
+			count++;
+		}
+
+	}
+
+}
